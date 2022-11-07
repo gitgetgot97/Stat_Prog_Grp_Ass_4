1fd22c4d4079cf6bbb5d393fe1289237af44a360
gitgetgot97 <114063220+gitgetgot97@users.noreply.github.com>
2022-11-07 12:46:36 +0000

    Add files via upload

---
+++ proj4.R
@@ -0,0 +1,13 @@
+# s1704264 Benedict Troy
+# s2451950 Scott Jenkins
+# s2453724 Emily Segers
+
+# Github Repo:
+# 
+
+# Contribution:
+# None of us contributed significantly more, or less, than the others. 
+# Hence we deserve equal marks on this assignment. 
+
+## Newton Optimizer
+
