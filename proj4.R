53f80adc2dc495a13e69b093e6d71a89c085f23e
ben-troy <s1704264@ed.ac.uk>
2022-11-13 14:27:58 +0000

    Initial newt function

--- proj4.R
+++ proj4.R
@@ -22,7 +22,7 @@ rb <- function(th,k=2) {
 k*(th[2]-th[1]^2)^2 + (1-th[1])^2
 }
 
-th <- c(2,1)
+th <- c(100,200)
 
 rb(th)
 
@@ -46,10 +46,6 @@ hb(th)
 
 
 
-## SJ: Trying to create grad and hess functions
-## For reference: there is package for this, but can't use it, because not in base R
-## https://cran.r-project.org/web/packages/EKMCMC/index.html
-
 rb <- function(th,k=2) {
   k*(th[2]-th[1]^2)^2 + (1-th[1])^2
 }
@@ -59,56 +55,77 @@ th <- c(2,1)
 th_1 <- c(4,1)
 rb(th); rb(th_1)
 
-## Want to differentiate rb wrt th[1] and th[2], grad is a vector of these. hess is the second partial derivatives
-
-deriv(rb(th), "th[1]")           ## these aren't working as I would expect. says grad = 0.
-deriv(rb(th), "th[2]")
-deriv(rb(th_1), "th[1]")
-eval(deriv(rb(th_1), "th[1]"))
 
 gb(th); gb(th_1)
 hb(th); hb(th_1)
 
 
-# A more simple example in one variable
-f = expression(x^2 + 5*x + 4)
-deriv(f, 'x')                                    ## QUESSTION: WE WANT .grad[,'x'] from here - how do we access it??
-x <- 1
-eval(f)   ## eval function to find the value at a given x
-eval(deriv(f,'x'))  
-deriv(deriv(f,'x'),'x')
-
-## Have a good weekend both!
-## Scott
 
 
-
-newt<- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
+newt<- function(theta,func,grad,hess=NULL,...,tol=1e-16,fscale=1,maxit=100,max.half=20,eps=1e-6){
+  ## theta is a vector of initial values for the optimization parameters.
+  ## func is the objective function to minimize. Its first argument is the vector of optimization parameters. 
+  ## Remaining arguments will be passed from newt using '...'.
+  ## grad is the gradient function. It has the same arguments as func 
+  ## but returns the gradient vector of the objective w.r.t. the elements of parameter vector.
+  ## hess is the Hessian matrix function. 
+  ## It has the same arguments as func but returns the Hessian matrix of the objective w.r.t. the elements of parameter vector. 
+  ## If not supplied then newt should obtain an approximation to the Hessian by finite differencing of the gradient vector (hint: (t(A)+A)/2 is exactly symmetric for any matrix A).
+  ## ... any arguments of func, grad and hess after the first (the parameter vector) are passed using this.
+  ## tol the convergence tolerance.
+  ## fscale a rough estimate of the magnitude of func near the optimum - used in convergence testing.
+  ## maxit the maximum number of Newton iterations to try before giving up.
+  ## max.half the maximum number of times a step should be halved before concluding that the step has failed to improve the objective.
+  ## eps the finite difference intervals to use when a Hessian function is not provided.
   
+  
+  ## newt should return a list containing:
+  ## f the value of the objective function at the minimum.
+  ## theta the value of the parameters at the minimum.
+  ## iter the number of iterations taken to reach the minimum.
+  ## g the gradient vector at the minimum (so the user can judge closeness to numerical zero).
+  ## Hi the inverse of the Hessian matrix at the minimum (useful if the objective is a negative log likelihood).
+  
+  iter <- 0
+  g <- grad(theta, ...)
+  hess0 <- hess(theta, ...)
+  
+  while(iter <= maxit 
+        & abs(max(grad(theta, ...))) > (tol*abs(func(theta,...)))+fscale){
+    
+    R <- chol(hess0)
+    id <- diag(length(theta))
+    hess_inv<-chol2inv(R)
+    
+    delta <- -(hess_inv)%*%g
+    temp_theta <- theta + delta
+    half_iter <- 1
+    
+    while(func(theta, ...) < func(temp_theta, ...) & half_iter <= max.half){
+      temp_theta <- theta + delta*(1/2 ^ half_iter)
+      half_iter <- half_iter + 1
+    }
+    
+    if(half_iter == max.half){
+      warning("Max half steps reached")
+    }
+    
+    theta <- temp_theta
+    f <- func(theta, ...)
+    g <- grad(theta, ...)
+    hess0 <- hess(theta, ...)
+    iter <- iter + 1
+  }
+  
+  R <- chol(hess0)
+  Hi <- chol2inv(R)
+  output <- list(theta = theta, f=f, iter=iter, g=g, Hi=Hi)
+  return(output) 
 }## End of newt function
 
-## theta is a vector of initial values for the optimization parameters.
-## func is the objective function to minimize. Its first argument is the vector of optimization parameters. 
-## Remaining arguments will be passed from newt using '...'.
-## grad is the gradient function. It has the same arguments as func 
-## but returns the gradient vector of the objective w.r.t. the elements of parameter vector.
-## hess is the Hessian matrix function. 
-## It has the same arguments as func but returns the Hessian matrix of the objective w.r.t. the elements of parameter vector. 
-## If not supplied then newt should obtain an approximation to the Hessian by finite differencing of the gradient vector (hint: (t(A)+A)/2 is exactly symmetric for any matrix A).
-## ... any arguments of func, grad and hess after the first (the parameter vector) are passed using this.
-## tol the convergence tolerance.
-## fscale a rough estimate of the magnitude of func near the optimum - used in convergence testing.
-## maxit the maximum number of Newton iterations to try before giving up.
-## max.half the maximum number of times a step should be halved before concluding that the step has failed to improve the objective.
-## eps the finite difference intervals to use when a Hessian function is not provided.
 
+trial <- newt(th, rb, gb, hess=hb)
 
-## newt should return a list containing:
-## f the value of the objective function at the minimum.
-## theta the value of the parameters at the minimum.
-## iter the number of iterations taken to reach the minimum.
-## g the gradient vector at the minimum (so the user can judge closeness to numerical zero).
-## Hi the inverse of the Hessian matrix at the minimum (useful if the objective is a negative log likelihood).
 
 
 
